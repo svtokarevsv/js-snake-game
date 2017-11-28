20192ebade7d577078fd0b984ad6e0a3486bc5a0
Svyatoslav <svtokarevsv@gmail.com>
2017-11-27 22:44:09 -0500

    new design implemented, added barriers and fishes are moving

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const game = function () {
 	let ctx
 	let partLength = 12
 	let frameRate = 10
+	let speed = 1
 	let direction
 	let previousDirection
 	let snake
@@ -10,14 +11,46 @@ const game = function () {
 	let score
 	let fruits
 	let fruitMax = 6
-	let headImage = new Image();
+	let loaded_images = 0
+	let currentShark
+	let barriers
+	let barriersMax = 2
+	const images = {
+		head: {src: '/img/head.png', object: null},
+		bubble: {src: '/img/Bubble.png', object: null, items: []},
+		background: {src: '/img/BG.jpg', object: null},
+		reef: {src: '/img/reef.png', object: null, size: 0.6},
+		shark_left: {src: '/img/Shark_left.png', object: null, size: 0.4, position: {x: null, y: null}},
+		shark_right: {src: '/img/Shark_right.png', object: null, size: 0.4, position: {x: null, y: null}},
+		fish1: {src: '/img/fishes/fish1.png', object: null},
+		fish2: {src: '/img/fishes/fish2.png', object: null},
+		fish3: {src: '/img/fishes/fish3.png', object: null},
+		fish4: {src: '/img/fishes/fish4.png', object: null},
+		fish5: {src: '/img/fishes/fish5.png', object: null},
+		hedgehog: {src: '/img/Hedgehog.png', object: null},
+	}
+
+	function loadImages(callback) {
+		for (let name in images) {
+			images[name].object = new Image();
+			images[name].object.src = images[name].src;
+			images[name].object.onload = () => itemLoaded(callback);
+		}
+	}
+
+	function itemLoaded(callback) {
+		loaded_images++
+		if (loaded_images >= Object.keys(images).length) {
+			callback();
+		}
+	}
+
 	//12px
-	headImage.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAZdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuMTM0A1t6AAAB90lEQVQoU2MIDQ1lZmVlTbCwUeuOSjH5GhKv+y0gRq4nLE2uJThO7VNksukvTz/DqQICAnn6+voKDPX19Uxm5maXD52Z+//pm4v/7z87/d/L1/G/pr7Y/+t3j/x//Prs/7PX1/4vLM79o6qqasAAAqk5fh3nbqz+f/Pemf8rVi/8n1Zo/T+3Uf3/2o2L/5++uPv/w+en/3dOzLkNVjx/vj1Hw3SDu9MXFv9fv6fsf0Gl3f/WqYr/22fK/88uV/8/aV7o/zU7iv5XdFl+mrLFVIJh4Rb96gU7jP+3z1P4n1Ym9r9xisz//sVKYNw1T/5/YbP4/6Jmyf8Tl2v9X7hFbx7Dwo26n5bv1v0/eZX6/7pJsv/rJ8v/n7pG8//0tVr/J61Q/V8zQe5//1LV/7M2aP1ftUv3H8OclUbnl2xS/z93uyF+vEnn/4ylevcZVm7zEC2s0Tg6a7PJ/8UHLLDipQes/jdO0L+7fIe/LNjjO3fGcC/Z4nRh0ynP/+tOuPxfc8IZiJ3+rwXSG066/990zPv3wg3OhmDFMLBoo7Vr72zr/wcuxf3ffSEYjPdeCP2/fl/Y/2lLLBdDlaGCtsk6fSV1Dv/zitT/N2Xo/I9Ol//fOdn9zvaDtpJQJZhg+Tpzz3leOldqalRW9c3Srdp2woMPKgUEDAwAlxoZ1KJ9mH0AAAAASUVORK5CYII='
 
 	function drawSnake() {
 		for (let part of snake) {
 			if (part.x === snake[0].x && part.y === snake[0].y) {
-				ctx.drawImage(headImage, part.x, part.y);
+				ctx.drawImage(images.head.object, part.x, part.y);
 			} else {
 				drawElem(part.x, part.y, 'snake');
 			}
@@ -43,9 +76,101 @@ const game = function () {
 	}
 
 	function drawFruits() {
+		if (!fruits.length) generateFruits();
 		for (let fruit of fruits) {
-			drawElem(fruit.x, fruit.y, 'fruit')
+
+			if (fruit.x > canvas.width || fruit.x < 0) fruit.path.leftRight = generateRandomBetween(0, 2);
+			if (fruit.y > canvas.height || fruit.y < 0) fruit.path.upDown = generateRandomBetween(0, 2);
+			fruit.path.distance--
+			if (fruit.path.distance === 0) {
+				fruit.path.upDown = generateRandomBetween(0, 2)
+				fruit.path.leftRight = generateRandomBetween(0, 2)
+				fruit.path.distance = generateRandomBetween(50, canvas.width / 2)
+				fruit.path.speed =  generateRandomBetween(1, 6)/10
+			}
+			let xShift = fruit.path.leftRight === 2 ? fruit.path.speed : fruit.path.leftRight === 1 ? 0 : -fruit.path.speed;
+			let yShift = fruit.path.upDown === 2 ? fruit.path.speed :  fruit.path.upDown === 1 ? 0 : -fruit.path.speed;
+			fruit.x += xShift
+			fruit.y += yShift
+			ctx.drawImage(images['fish' + fruit.type].object, fruit.x - partLength / 4, fruit.y - partLength / 4,
+				partLength * 1.5, partLength * 1.5);
 		}
+	}
+
+	function getImgDimensions(img) {
+		let img_ratio = img.object.width / img.object.height
+		let width = img.size * canvas.width;
+		let height = width / img_ratio;
+		return {width, height}
+	}
+
+	function drawReef() {
+		const {width, height}=getImgDimensions(images.reef)
+		ctx.drawImage(images.reef.object, canvas.width - width, canvas.height - height, width, height);
+	}
+
+	function drawShark() {
+		currentShark = currentShark ? currentShark : images.shark_left
+		const {width, height}=getImgDimensions(currentShark)
+		let x = currentShark.position.x
+		if (x === null || x < (0 - width) || x > canvas.width) {
+			let type = generateRandomBetween(0, 1)
+			currentShark = type ? images.shark_left : images.shark_right
+			currentShark.position.y = generateRandomBetween(0, canvas.height - height);
+			currentShark.position.x = currentShark.src === images.shark_left.src ? canvas.width : 0
+		} else {
+			currentShark.position.x = currentShark.src === images.shark_left.src ?
+				currentShark.position.x - 0.7 * speed : currentShark.position.x + 0.7 * speed
+		}
+		ctx.drawImage(currentShark.object, currentShark.position.x, currentShark.position.y, width, height);
+	}
+
+	function drawBubbles() {
+
+		let chance = generateRandomBetween(0, 39) > 38;
+		if (chance) {
+			let x = generateRandomBetween(0, canvas.width)
+			let size = generateRandomBetween(3, 14)
+			let speed = generateRandomBetween(0.1, 1.5)
+			let direction = generateRandomBetween(0, 2)
+			images.bubble.items.push({x, y: canvas.height, height: size, width: size, speed, direction})
+		}
+		for (let i = images.bubble.items.length - 1; i >= 0; i--) {
+			let item = images.bubble.items[i]
+			if (item.y < 0) {
+				images.bubble.items[i] = null
+				images.bubble.items.splice(i, 1)
+				continue
+			}
+			item.y -= item.speed
+			let move = generateRandomBetween(0, 1)
+
+			switch (item.direction) {
+				default:
+					break
+				case 1:
+					item.x += move
+					break
+				case 2:
+					item.x -= move
+					break
+			}
+			ctx.drawImage(images.bubble.object, item.x, item.y, item.height, item.width);
+		}
+	}
+
+	function drawBackground() {
+		drawShark()
+		drawReef()
+		drawBubbles()
+	}
+
+	function drawBarriers() {
+		if (!barriers.length) generateBarriers();
+		for (let barrier of barriers) {
+			ctx.drawImage(barrier.object, barrier.x, barrier.y, barrier.width, barrier.width);
+		}
+
 	}
 
 	function draw() {
@@ -53,32 +178,68 @@ const game = function () {
 		if (isCollision()) {
 			return endGame();
 		}
+		drawBackground();
+		drawBarriers();
 		drawFruits();
 		drawSnake();
 	}
 
 	function isFruitEaten(x, y) {
 		let eatenIndex
-		let isEaten = fruits.some((part, index) => {
+		let isEaten = fruits.some((fruit, index) => {
 			eatenIndex = index
-			return part.x == x && part.y == y
+			return isInsideElement({x, y},fruit )||isInsideElement(fruit,{x, y} )
 		})
 		if (isEaten) {
 			fruits.splice(eatenIndex, 1)
-			generateFruit(x, y)
+			generateFruits()
 		}
 		return isEaten
 	}
 
-	function generateFruit(x, y) {
-		for (let i = fruits.length; i < fruitMax; i++) {//create new fruits if there is less than fruitMax of them
+	function generateRandomBetween(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min)
+	}
+
+	function generateFruits() {
+		for (let i = fruits.length; i < fruitMax; i++) {//create new fruits if there is less than fruitsmax of them
 			let fruitX
 			let fruitY
+			let type
+			let path
 			do {//define if created fruit is inside of snake
-				fruitX = Math.round(Math.floor(Math.random() * (canvas.width)) / partLength) * partLength
-				fruitY = Math.round(Math.floor(Math.random() * (canvas.height)) / partLength) * partLength
-			} while (x == fruitX && y == fruitY)
-			fruits.push({x: fruitX, y: fruitY})
+				fruitX = Math.round(generateRandomBetween(0, canvas.width) / partLength) * partLength
+				fruitY = Math.round(generateRandomBetween(0, canvas.height) / partLength) * partLength
+				type = generateRandomBetween(1, 5)
+				path = {
+					upDown: generateRandomBetween(0, 2),
+					leftRight: generateRandomBetween(0, 2),
+					distance: generateRandomBetween(partLength*3, partLength*6),
+					speed: generateRandomBetween(1, 6)/10
+				}
+			} while (snake.some(part => isInsideElement({x: fruitX, y: fruitY}, part)) ||
+			barriers.some(barrier => isInsideElement({x: fruitX, y: fruitY}, barrier)))
+			fruits.push({x: fruitX, y: fruitY, type, path})
+		}
+	}
+
+	function isInsideElement(newObj, existingObj) {
+		let width = existingObj.width || partLength
+		return newObj.x >= existingObj.x && newObj.x < (existingObj.x + width) &&
+			newObj.y >= existingObj.y && newObj.y < (existingObj.y + width)
+	}
+
+	function generateBarriers() {
+		for (let i = 0; i < barriersMax; i++) {//create new fruits if there is less than fruitsmax of them
+			let barrierX
+			let barrierY
+			// let type
+			do {//define if created fruit is inside of snake
+				barrierX = Math.round(generateRandomBetween(0, (canvas.width - partLength * 4)) / partLength) * partLength
+				barrierY = Math.round(generateRandomBetween(0, (canvas.height - partLength * 4)) / partLength) * partLength
+				// type = Math.floor(Math.random() * (5 - 1) + 1)
+			} while (barriers.some(barrier => isInsideElement({x: barrierX, y: barrierY}, barrier)))
+			barriers.push({x: barrierX, y: barrierY, object: images.hedgehog.object, width: partLength * 4})
 		}
 	}
 
@@ -86,6 +247,7 @@ const game = function () {
 		snake = []
 		score = 0
 		fruits = []
+		barriers = []
 		direction = 'right'
 		previousDirection = 'right'
 		canvas.width = canvas.height = partLength * 40;
@@ -101,15 +263,6 @@ const game = function () {
 		for (let i = 0; i < 4; i++) {
 			snake.unshift({x: i * partLength, y: 0, direction});
 		}
-		for (let i = 0; i < fruitMax; i++) {//create new fruits if there is less than fruitsmax of them
-			let fruitX
-			let fruitY
-			do {//define if created fruit is inside of snake
-				fruitX = Math.round(Math.floor(Math.random() * (canvas.width)) / partLength) * partLength
-				fruitY = Math.round(Math.floor(Math.random() * (canvas.height)) / partLength) * partLength
-			} while (snake.some(part => part.x == fruitX && part.y == fruitY))
-			fruits.push({x: fruitX, y: fruitY})
-		}
 		gameLoop = setInterval(draw, frameRate);
 	}
 
@@ -117,25 +270,13 @@ const game = function () {
 		let disabled = false;
 		window.addEventListener('keydown', function (ev) {
 			let key = ev.keyCode;
-			chooseDirection(key, ev)
+			manageKeys(key, ev)
 		})
 		window.addEventListener('click', function (ev) {
 			let id = ev.target.id;
-			chooseDirection(id, ev)
-			if (disabled)return
-			if (id === 'play') {
-				disabled = true;
-				setTimeout(() => disabled = false, 100)
-				if (!gameLoop) {
-					ev.target.src = '../img/pause.png'
-					return gameLoop = setInterval(draw, frameRate);
-				}
-				ev.target.src = '../img/play.png'
-				clearInterval(gameLoop);
-				gameLoop = null;
-			}
+			manageKeys(id, ev)
 		})
-		function chooseDirection(key, ev) {
+		function manageKeys(key, ev) {
 			if (disabled)return;
 			switch (key) {
 				case 37:
@@ -162,6 +303,17 @@ const game = function () {
 					ev.preventDefault()
 					if (direction !== 'up') direction = 'down'
 					break;
+				case 32:
+				case 'play':
+					ev.preventDefault()
+					if (!gameLoop) {
+						ev.target.src = '../img/pause.png'
+						return gameLoop = setInterval(draw, frameRate);
+					}
+					ev.target.src = '../img/play.png'
+					clearInterval(gameLoop);
+					gameLoop = null;
+					break;
 				default:
 					return;
 					break;
@@ -170,6 +322,7 @@ const game = function () {
 			setTimeout(() => disabled = false, frameRate * 6)
 		}
 	}
+
 
 	function endGame() {
 		clearInterval(gameLoop)
@@ -212,16 +365,16 @@ const game = function () {
 			let part = snake[i]
 			switch (part.direction) {
 				case 'right':
-					part.x++
+					part.x += speed
 					break;
 				case 'left':
-					part.x--
+					part.x -= speed
 					break;
 				case 'up':
-					part.y--
+					part.y -= speed
 					break;
 				case 'down':
-					part.y++
+					part.y += speed
 					break;
 			}
 		}
@@ -239,11 +392,12 @@ const game = function () {
 		let outOfWidth = snakeX >= canvas.width || snakeX < 0;
 		let outOfHeight = snakeY >= canvas.height || snakeY < 0;
 		let isSnakeEatingItself = snake.filter(part => part.x === snakeX && part.y === snakeY).length >= 2;
-		return outOfHeight || outOfWidth || isSnakeEatingItself;
+		let isBarrierTouched = barriers.some(barrier => isInsideElement({x: snakeX, y: snakeY}, barrier))
+		return outOfHeight || outOfWidth || isSnakeEatingItself || isBarrierTouched;
 	}
 
 	function run() {
-		init();
+		loadImages(init);
 		initControls();
 	}
 
